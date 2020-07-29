e78e645d4bbafe027f9340f6fc301cd6579762ad
Adia Wahyu Dwi Prasetya <54004272+adiakdcode@users.noreply.github.com>
2020-07-29 09:40:47 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,38 +6,39 @@ else
   console.log(`Workbox gagal dimuat`);
 
 workbox.precaching.precacheAndRoute([
-  { url: './manifest.json', revision: '2' },
-  { url: './index.html', revision: '2' },
-  { url: './pages/fav-team.html', revision: '2' },
-  { url: './pages/home.html', revision: '2' },
-  { url: './pages/match.html', revision: '2' },
-  { url: './pages/team.html', revision: '2' },
-  { url: './pages/topscore.html', revision: '2' },
-  { url: './assets/shell/nav-shell.html', revision: '2' },
-  { url: './assets/images/epl_icon1.png',  revision: '2' },
-  { url: './assets/images/epl_club.jpg',  revision: '2' },
-  { url: './assets/images/standing_bg.jpg',  revision: '2' },
-  { url: './assets/images/pl_bg.jpg',  revision: '2' },
-  { url: './assets/images/epl_sd.jpg', revision: '2' },
-  { url: './assets/images/icon.png', revision: '2' },
-  {url : './assets/images/myicon.png', revision: '2' },
-  { url: './assets/images/icons/icon-96x96.png', revision: '2' },
-  { url: './assets/images/icons/icon-128x128.png', revision: '2' },
-  { url: './assets/images/icons/icon-144x144.png', revision: '2' },
-  { url: './assets/images/icons/icon-152x152.png', revision: '2' },
-  { url: './assets/images/icons/icon-192x192.png', revision: '2' },
-  { url: './assets/images/icons/icon-384x384.png', revision: '2' },
-  { url: './assets/images/icons/icon-512x512.png', revision: '2' },
-  { url: './assets/css/materialize.css', revision: '2' },
-  { url: './assets/css/materialize.min.css', revision: '2' },
-  { url: './assets/css/style.css', revision: '2' },
-  { url: './assets/js/api.js', revision: '2' },
-  { url: './assets/js/DB.js', revision: '2' },
-  { url: './assets/js/nav.js', revision: '2' },
-  { url: './assets/js/idb.js', revision: '2' },
-  { url: './assets/js/init.js', revision: '2' },
-  { url: './assets/js/materialize.min.js', revision: '2' },
-  { url: './assets/js/script.js', revision: '2' },
+  { url: './manifest.json', revision: '3' },
+  { url: './index.html', revision: '3' },
+  { url: './pages/fav-team.html', revision: '3' },
+  { url: './pages/home.html', revision: '3' },
+  { url: './pages/match.html', revision: '3' },
+  { url: './pages/team.html', revision: '3' },
+  { url: './pages/topscore.html', revision: '3' },
+  { url: './assets/shell/nav-shell.html', revision: '3' },
+  { url: './assets/images/epl_icon1.png',  revision: '3' },
+  { url: './assets/images/epl_club.jpg',  revision: '3' },
+  { url: './assets/images/standing_bg.jpg',  revision: '3' },
+  { url: './assets/images/pl_bg.jpg',  revision: '3' },
+  { url: './assets/images/epl_sd.jpg', revision: '3' },
+  { url: './assets/images/icon.png', revision: '3' },
+  { url : './assets/images/myicon.png', revision: '3' },
+  { url: './assets/images/icons/icon-96x96.png', revision: '3' },
+  { url: './assets/images/icons/icon-128x128.png', revision: '3' },
+  { url: './assets/images/icons/icon-144x144.png', revision: '3' },
+  { url: './assets/images/icons/icon-152x152.png', revision: '3' },
+  { url: './assets/images/icons/icon-192x192.png', revision: '3' },
+  { url: './assets/images/icons/icon-384x384.png', revision: '3' },
+  { url: './assets/images/icons/icon-512x512.png', revision: '3' },
+  { url: './assets/css/materialize.css', revision: '3' },
+  { url: './assets/css/materialize.min.css', revision: '3' },
+  { url: './assets/css/style.css', revision: '3' },
+  { url: './assets/js/api.js', revision: '3' },
+  { url: './assets/js/DB.js', revision: '3' },
+  { url: './assets/js/nav.js', revision: '3' },
+  { url: './assets/js/idb.js', revision: '3' },
+  { url: './assets/js/init.js', revision: '3' },
+  { url: './assets/js/materialize.min.js', revision: '3' },
+  { url: './assets/js/script.js', revision: '3' },
+  { url: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', revision: '3'}
 ], {
   ignoreUrlParameterMatching: [/.*/]
 });
@@ -45,25 +46,18 @@ workbox.precaching.precacheAndRoute([
 workbox.routing.registerRoute(
   new RegExp('https://api.football-data.org/v2/'),
   workbox.strategies.staleWhileRevalidate()
-);
+)
 
 workbox.routing.registerRoute(
   /.*(?:googleapis|gstatic)\.com/,
   workbox.strategies.staleWhileRevalidate({
     cacheName: 'google-fonts-stylesheets',
   })
-);
+)
 
 workbox.routing.registerRoute(
-    new RegExp('/pages/'),
-      workbox.strategies.staleWhileRevalidate({
-          cacheName: 'halaman'
-    })
-);
-
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.cacheFirst({
+  ({url}) => url.origin,
+  workbox.strategies.networkFirst({
     cacheName: 'images',
     plugins: [
       new workbox.expiration.Plugin({
@@ -71,8 +65,8 @@ workbox.routing.registerRoute(
         maxAgeSeconds: 14 * 24 * 60 * 60, // 14 hari
       }),
     ],
-  }),
-);
+  })
+)
 
 // Push notification
 self.addEventListener('push', event => {
